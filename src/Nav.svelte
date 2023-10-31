5e9929a151bfc07c52e463ba2ac51e133d67f081
Adam Stachowicz <saibamenppl@gmail.com>
2023-10-31 10:13:59 +0100

    Working version 2.1.0

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -1,9 +1,16 @@
 <script>
-import {Link} from 'svelte-routing'
+import { Router, Link } from 'svelte-routing'
+import Navigation from './Navigation.svelte';
 </script>
 
-<nav>
+<Router>
+	<Navigation />
+	<Navigation items={[
+		{ text: "Batches list", url: "/batch/list/1" },
+        { text: "Batch #1", url: "/batch/1" },
+        { text: "Regenerate DATA files" },
+	]} />
 	<Link to="/">Home</Link>
 	<Link to="/about">About</Link>
 	<Link to="/blog">Blog</Link>
-</nav>
+</Router>
---
+++ src/Navigation.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { Router, Link} from "svelte-routing";
+
+    export let items = [];
+    export let separator = ">";
+</script>
+
+<div id="nav">
+    <Router>
+        <span class="item">
+            {#if items && items.length > 0}
+                <a href="/">Home</a>
+            {:else}
+                Home
+            {/if}
+        </span>
+        {#each items as item, i}
+            <span>{separator}</span>
+            {#if (items.length - 1) === i}
+                <span class="item">{item.text}</span>
+            {:else}
+                <span class="item"><Link to="{item.url}">{item.text}</Link></span>
+            {/if}
+        {/each}
+    </Router>
+</div>
+
+<style>
+:global(#nav) {
+    margin-bottom: 15px;
+}
+:global(#nav span) {
+    margin-left: 0px;
+    margin-right: 5px;
+}
+</style>
